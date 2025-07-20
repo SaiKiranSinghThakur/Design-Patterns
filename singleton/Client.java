0abd37a5f1e3460a53808f82e540ca4deb10b4b0
Sai Kiran Singh Thakur <saikiran7505@gmail.com>
2025-07-20 17:17:27 -0400

    Singleton Pattern.

---
+++ singleton/Client.java
@@ -0,0 +1,19 @@
+package singleton;
+
+public class Client {
+
+	public static void main(String[] args) {
+		PrinterService worker1 = PrinterService.getInstance();
+		PrinterService worker2 = PrinterService.getInstance();
+
+		worker1.setMode("Color");
+		worker2.setMode("Grayscale");
+
+		String worker1Mode = worker1.getPrinterStatus();
+		String workerMode = worker2.getPrinterStatus();
+
+		System.out.println(worker1Mode);
+		System.out.println(workerMode);
+	}
+
+}
